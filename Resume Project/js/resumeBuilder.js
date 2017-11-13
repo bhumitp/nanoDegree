cf49ea09f19db29085164a9c73de3fe0a36d283a
PATEL <bp801h@att.com>
2017-11-13 11:30:33 -0500

    updated details

--- Resume Project/js/resumeBuilder.js
+++ Resume Project/js/resumeBuilder.js
@@ -1,3 +1,144 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//Education Object
+var education = {
+  "schools": [{
+    "name": "KSU",
+    "city": "Marietta, GA",
+    "degree": "BS",
+    "major": "CompSci",
+    "dates": 2013,
+    "url": "http://www.ksu.edu"
+  }],
+  "onlineCourses": [{
+    "title": "JavaScript Crash Course",
+    "school": "udacity",
+    "dates": 2014,
+    "url": "http://www.udacity.com/course/ud804"
+  }]
+}
+
+//Bio Object
+var bio = {
+  "name": "Bhumit Patel",
+  "role": "Web Developer",
+  "contacts": {
+    "mobile": "650-555-5555",
+    "email": "john@example.com",
+    "github": "johndoe",
+    "twitter": "@johnDoe",
+    "location": "Atlanta"
+  },
+  "welcomeMessage": "Hello there!! welcome...",
+  "skills": ["awesomeness", "programming", "teaching", "JS"],
+  "bioPic": "images/fry.jpg"
+}
+
+//work Object
+var work = {
+  "jobs": [
+    {
+      "employer": "Udacity",
+      "title": "Course Developer",
+      "location": "Mountain View, CA",
+      "dates": "Feb 2014 - Current",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "LearnBIG",
+      "title": "Software Engineer",
+      "location": "Seattle, WA",
+      "dates": "May 2013 - Jan 2014",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "LEAD Academy Charter High School",
+      "title": "Science Teacher",
+      "location": "Nashville, TN",
+      "dates": "Jul 2012 - May 2013",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    },
+    {
+      "employer": "Stratford High School",
+      "title": "Science Teacher",
+      "location": "Nashville, TN",
+      "dates": "Jun 2009 - Jun 2012",
+      "description": "Who moved my cheese cheesy feet cauliflower cheese. Queso taleggio when the cheese comes out everybody's happy airedale ricotta cheese and wine paneer camembert de normandie. Swiss mozzarella cheese slices feta fromage frais airedale swiss cheesecake. Hard cheese blue castello halloumi parmesan say cheese stinking bishop jarlsberg."
+    }
+  ]
+}
+
+//project Object
+var projects = {
+  "projects": [{
+    "title": "Sample project",
+    "dates": "2014",
+    "description": "Did prject for school assignment",
+    "images": [
+      "https://www.google.com",
+      "https://www.yahoo.com"
+    ]
+  }]
+}
+
+//Adding code for template
+function displayWork() {
+  for (job in work.jobs) {
+    //Adding work experience
+    $("#workExperience").append(HTMLworkStart);
+
+    //Formatting work experience and title
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+    //Formatting dates
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates);
+
+    //Formatting description
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-entry:last").append(formattedDescription);
+  }
+}
+
+displayWork();
+
+projects.display = function(){
+  for(project in projects.projects){
+    $("#projects").append(HTMLprojectStart);
+
+    var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+    $(".project-entry:last").append(formattedTitle);
+
+    var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+    $(".project-entry:last").append(formattedDates);
+
+    var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+    $(".project-entry:last").append(formattedDescription);
+
+    if (projects.projects[project].images.length > 0) {
+      for(image in projects.projects[project].images){
+        var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+        $(".project-entry:last").append(formattedImage);
+      }
+    }
+  }
+}
+// //Collecting click locations
+// $(document).click(function(loc){
+//   var x = loc.pageX;
+//   var y = loc.pageY;
+//
+//   logClicks(x,y);
+// });
+//
+// function locationizer(work_obj){
+//   var locationArray = [];
+//
+//   for (job in work_obj.jobs){
+//     var newLocation = work_obj.jobs[job].location;
+//     locationArray.push(newLocation);
+//   }
+//   return locationArray;
+// }
+// console.log(locationizer(work));
