53f7331e92e42c2b00451def611e52fbbcdccea4
bhumitp <30988270+bhumitp@users.noreply.github.com>
2017-12-21 17:21:53 -0500

    updated files

--- neighborhood-map/js/app.js
+++ neighborhood-map/js/app.js
@@ -8,11 +8,11 @@ function initMap() {
 
   //New Map
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 6,
+    zoom: 10,
     center: coordinates
   });
 
-  // // Adding starting Marker
+  // Adding starting Marker
   // var marker = new google.maps.Marker({
   //   position: coordinates,
   //   map: map
@@ -27,12 +27,50 @@ function initMap() {
   // marker.addListener('click', function(){
   //   infoWindow.open(map, marker);
   // });
-  addMarker(coordinates);
-  //Add marker
-  function addMarker(coordinates){
-    var marker = new google.maps.Marker({
-      position: coordinates,
-      map: map
-    });
+
+  var locations = [{
+      title: 'Fox Theatre',
+      coordinates:{lat: 33.7726059, lng: -84.3852046},
+      type: 'Entertainment'
+    },
+    {
+      title: 'CNN Studios',
+      coordinates:{lat: 33.7580, lng: -84.3951},
+      type: 'Entertainment'
+    },
+    {
+      title: 'Coke Factory',
+      coordinates:{lat: 33.7509397, lng: -84.3890936},
+      type: 'Entertainment'
+    },
+    {
+      title: 'GA Aquarium',
+      coordinates:{lat: 33.7622222, lng: -84.395},
+      type: 'Entertainment'
+    }
+  ]
+
+  for(var i = 0; i < myLocations.length; i++){
+    this.markerTitle = myLocations[i].title;
+            this.markerLat = myLocations[i].lat;
+            this.markerLng = myLocations[i].lng;
+            var marker = new google.maps.Marker({
+              position: {lat: this.markerLat,
+                    lng: this.markerLng
+                  },
+              map: map
+            });
+    //addMarker(myLocations[i]);
   }
+
+  //addMarker(coordinates);
+  // addMarker({coordinates:{lat:42.4668,lng:-70.9495}});
+  // addMarker({coordinates:{lat:42.8584,lng:-70.9300}});
+
+  // function addMarker(props) {
+  //   var marker = new google.maps.Marker({
+  //     position: props.coordinates,
+  //     map: map
+  //   });
+  // }
 }
---
+++ neighborhood-map/js/markers.js
@@ -0,0 +1,56 @@
+var myLocations = [
+    {
+        title: 'Fox Theatre',
+        lat: 33.7726059,
+        lng: -84.3852046,
+        type: 'Entertainment'
+    },
+    {
+        title: 'CNN Studios',
+        lat: 33.7580,
+        lng: -84.3951,
+        type: 'Entertainment'
+    },
+    {
+        title: 'Coke Factory',
+        lat: 33.7509397,
+        lng: -84.3890936,
+        type:  'Entertainment'
+    },
+    {
+        title: 'GA Aquarium',
+        lat: 33.7622222,
+        lng: -84.395,
+        type:  'Entertainment'
+    },
+    {
+        title: 'Phipps Plaza',
+        lat: 33.8525,
+        lng: 84.3620,
+        type: 'Shopping'
+    },
+    {
+        title: 'Lenox Mall',
+        lat: 33.8463,
+        lng: -84.3621,
+        type: 'Shopping'
+    },
+    {
+        title: 'Sun Trust Park',
+        lat: 33.8907,
+        lng: -84.4677,
+        type: 'Sports'
+    },
+    {
+        title: 'Phillips Arena',
+        lat: 33.7573,
+        lng: -84.3963,
+        type: 'Sports'
+    },
+    {
+        title: 'Shake Shack',
+        lat: 33.8377,
+        lng: -84.3809,
+        type: 'Restaurant'
+    }
+]
