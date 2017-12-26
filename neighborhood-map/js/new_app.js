824fa4465274c64a0a59bee2b71a6787467a5edb
bhumitp <30988270+bhumitp@users.noreply.github.com>
2017-12-26 12:50:31 -0500

    minor changes

--- neighborhood-map/js/new_app.js
+++ neighborhood-map/js/new_app.js
@@ -1,66 +1,6 @@
 //Initialize the map
 var map;
 
-//Initialize the array for location/markers
-//setting up locations
-// var myLocations = [
-//     {
-//         title: 'Fox Theatre',
-//         lat: 33.7726059,
-//         lng: -84.3852046,
-//         type: 'Entertainment'
-//     },
-//     {
-//         title: 'CNN Studios',
-//         lat: 33.7580,
-//         lng: -84.3951,
-//         type: 'Entertainment'
-//     },
-//     {
-//         title: 'Coke Factory',
-//         lat: 33.7509397,
-//         lng: -84.3890936,
-//         type:  'Entertainment'
-//     },
-//     {
-//         title: 'GA Aquarium',
-//         lat: 33.7622222,
-//         lng: -84.395,
-//         type:  'Entertainment'
-//     },
-//     {
-//         title: 'Phipps Plaza',
-//         lat: 33.8525,
-//         lng: 84.3620,
-//         type: 'Shopping'
-//     },
-//     {
-//         title: 'Lenox Mall',
-//         lat: 33.8463,
-//         lng: -84.3621,
-//         type: 'Shopping'
-//     },
-//     {
-//         title: 'Sun Trust Park',
-//         lat: 33.8907,
-//         lng: -84.4677,
-//         type: 'Sports'
-//     },
-//     {
-//         title: 'Phillips Arena',
-//         lat: 33.7573,
-//         lng: -84.3963,
-//         type: 'Sports'
-//     },
-//     {
-//         title: 'Shake Shack',
-//         lat: 33.8377,
-//         lng: -84.3809,
-//         type: 'Restaurant'
-//     }
-// ]
-
-
 //Initialize coordinates
 var coordinates = {
   lat: 33.7490,
@@ -70,69 +10,56 @@ var coordinates = {
 // Initialize the map
 var map;
 function initMap() {
-    "use strict";
     map = new google.maps.Map(document.getElementById('map'), {
         center: coordinates,
         zoom: 11,
-        disableDefaultUI: true
+        //disableDefaultUI: true
     });
-    // Start the ViewModel here so it doesn't initialize before Google Maps loads
+
+    // Initializing the ko view model
     ko.applyBindings(new ViewModel());
 }
 
-// Alert the user if google maps isn't working
+// Alert if maps api fail
 function googleError() {
-    "use strict";
-    document.getElementById('error').innerHTML = "<h2>Google Maps is not loading. Please try refreshing the page later.</h2>";
+    alert('There seems to be an issue with Google Maps API, Please try again later.');
 }
 
-// Place constructor
-// Credit https://discussions.udacity.com/t/having-trouble-accessing-data-outside-an-ajax-request/39072/10
+// Building the constructor
 var Place = function (data) {
-    "use strict";
     this.title = ko.observable(data.title);
     this.lat = ko.observable(data.lat);
     this.lng = ko.observable(data.lng);
-    this.id = ko.observable(data.id);
     this.marker = ko.observable();
     this.phone = ko.observable('');
     this.description = ko.observable('');
     this.address = ko.observable('');
     this.rating = ko.observable('');
-    this.url = ko.observable('');
-    this.canonicalUrl = ko.observable('');
-    this.photoPrefix = ko.observable('');
-    this.photoSuffix = ko.observable('');
     this.contentString = ko.observable('');
 };
 
 // ViewModel
 var ViewModel = function () {
-    "use strict";
-    // Make this accessible
-    var self = this;
 
-    // Create an array of all places
-    // Credit https://www.udacity.com/course/viewer#!/c-ud989-nd/l-3406489055/e-3464818693/m-3464818694
+  var self = this;
+
+    // Creating array of all locations
     this.placeList = ko.observableArray([]);
 
-    // Call the Place constructor
-    // Create Place objects for each item in locations & store them in the above array
-    // Credit https://www.udacity.com/course/viewer#!/c-ud989-nd/l-3406489055/e-3464818693/m-3464818694
+    // Creating objects for each marker/location
     myLocations.forEach(function (placeItem) {{}
         self.placeList.push(new Place(placeItem));
     });
 
-    // Initialize the infowindow
+    // Initializing the infowindow
     var infowindow = new google.maps.InfoWindow({
         maxWidth: 200,
     });
 
-    // Initialize marker
+    // Initializing marker
     var marker;
 
-    // For each place, set markers, request Foursquare data, and set event listeners for the infowindow
-    // Credit https://github.com/kacymckibben/project-5-app.git
+    // Setting markers and requesting api data
     self.placeList().forEach(function (placeItem) {
 
         // Define markers for each place
@@ -143,14 +70,23 @@ var ViewModel = function () {
         });
         placeItem.marker = marker;
 
+        // Setting up varibales for api call
+
+        //Foursquare api client ID
         var clientID = "SDAWFJVJMXDH011Q0JCQLB54FG4H232CSMAXO4HAJBC0KQYO";
-        var clientSecret =
-            "LGTNR2W3Q1KUWC4DZNEHLP0UK1K3RX0OO1KAA30IWB1PLVPH";
+        //Foursquare api client secret key
+        var clientSecret = "LGTNR2W3Q1KUWC4DZNEHLP0UK1K3RX0OO1KAA30IWB1PLVPH";
+        //Storing the latitude in variable
         var lat = placeItem.lat();
         console.log(lat);
+        //Storing longitude in variable
         var lng = placeItem.lng();
+        console.log(lng);
+        //Storing title in variable
         var title = placeItem.title();
-        // Make AJAX request to Foursquare
+        console.log(title);
+
+        // AJAX request to Foursquare
         $.ajax({
             url: 'https://api.foursquare.com/v2/venues/' + lat + ',' + lng +
             '&client_id=' + clientID +
@@ -175,23 +111,14 @@ var ViewModel = function () {
                     placeItem.address(location.address || '');
                 }
 
-                var bestPhoto = result.hasOwnProperty('bestPhoto') ? result.bestPhoto : '';
-                if (bestPhoto.hasOwnProperty('prefix')) {
-                    placeItem.photoPrefix(bestPhoto.prefix || '');
-                }
-
-                if (bestPhoto.hasOwnProperty('suffix')) {
-                    placeItem.photoSuffix(bestPhoto.suffix || '');
-                }
-
                 var description = result.hasOwnProperty('description') ? result.description : '';
                 placeItem.description(description || '');
 
                 var rating = result.hasOwnProperty('rating') ? result.rating : '';
                 placeItem.rating(rating || 'none');
 
-                var url = result.hasOwnProperty('url') ? result.url : '';
-                placeItem.url(url || '');
+                // var url = result.hasOwnProperty('url') ? result.url : '';
+                // placeItem.url(url || '');
 
                 placeItem.canonicalUrl(result.canonicalUrl);
 
@@ -200,20 +127,19 @@ var ViewModel = function () {
                 // Credit https://discussions.udacity.com/t/trouble-with-infowindows-and-contentstring/39853/14
 
                 // Content of the infowindow
-                var contentString = '<div id="iWindow"><h4>' + placeItem.name() + '</h4><div id="pic"><img src="' +
-                        placeItem.photoPrefix() + '110x110' + placeItem.photoSuffix() +
-                        '" alt="Image Location"></div><p>Information from Foursquare:</p><p>' +
+                var contentString = '<div id="iWindow"><h4>' + placeItem.name() +
                         placeItem.phone() + '</p><p>' + placeItem.address() + '</p><p>' +
                         placeItem.description() + '</p><p>Rating: ' + placeItem.rating() +
-                        '</p><p><a href=' + placeItem.url() + '>' + placeItem.url() +
-                        '</a></p><p><a target="_blank" href=' + placeItem.canonicalUrl() +
-                        '>Foursquare Page</a></p><p><a target="_blank" href=https://www.google.com/maps/dir/Current+Location/' +
-                        placeItem.lat() + ',' + placeItem.lng() + '>Directions</a></p></div>';
+                      //'</p><p><a href=' + placeItem.url() + '>' + placeItem.url() +
+                        '</a></p><p><a target="_blank" href=' + placeItem.canonicalUrl();
+                        //  +
+                        // '>Foursquare Page</a></p><p><a target="_blank" href=https://www.google.com/maps/dir/Current+Location/' +
+                        // placeItem.lat() + ',' + placeItem.lng() + '>Directions</a></p></div>';
 
-                // Add infowindows credit http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
+                // Adding the info windows
+                // credit http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
                 google.maps.event.addListener(placeItem.marker, 'click', function () {
                     infowindow.open(map, this);
-                    // Bounce animation credit https://github.com/Pooja0131/FEND-Neighbourhood-Project5a/blob/master/js/app.js
                     placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
                     setTimeout(function () {
                         placeItem.marker.setAnimation(null);
